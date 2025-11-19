017552c5286212981bb153a887cbe2daa8632221
Denys Septian <denys.septian89@gmail.com>
2025-11-20 03:56:10 +0700

    fix url program

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -63,8 +63,8 @@
                                     </li>
                                     <li class="dropdown"><a href="#">Unit Sekolah</a>
                                         <ul>
-                                            <li><a href="{{ route('programs.show', ['slug' => 'kb-tk-islam-al-munawwar']) }}">TK Islam Al Munawwar</a></li>
-                                            <li><a href="{{ route('programs.show', ['slug' => 'sd-islam-al-munawwar']) }}">SD Islam Al Munawwar</a></li>
+                                            <li><a href="{{ route('programs.show', ['slug' => 'kb-tk-islam-al-munawwar']) }}">KB-TK Islam Al Munawwar</a></li>
+                                            <li><a href="{{ route('programs.show', ['slug' => 'sdit-islam-al-munawwar']) }}">SDIT Islam Al Munawwar</a></li>
                                             <li><a href="{{ route('programs.show', ['slug' => 'rumah-kasih-sayang-untuk-anak-yatim']) }}">Panti Al Munawwar</a></li>
                                             <li><a href="{{ route('programs.show', ['slug' => 'masjid-al-munawwar']) }}">Masjid Al Munawwar</a></li>
                                         </ul>
